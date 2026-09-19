722930c3ad273fac5c3a5735be9d1ad76931fc35
Afuwape Babatunde <afutunde@gmail.com>
2026-09-19 02:55:38 +0100

    chore: update platform/frontend/src/design-system/index.ts

---
+++ platform/frontend/src/design-system/index.ts
@@ -0,0 +1,31 @@
+// Core UI Components
+export * from "./components/Button"
+export * from "./components/StatusBadge"
+export * from "./components/Input"
+export * from "./components/Field"
+export * from "./components/SearchInput"
+export * from "./components/MoneyInput"
+export * from "./components/PhoneInput"
+export * from "./components/Checkbox"
+export * from "./components/Switch"
+export * from "./components/Dialog"
+export * from "./components/InlineAlert"
+export * from "./components/EmptyState"
+export * from "./components/Skeleton"
+
+// Product Patterns
+export * from "./patterns/PackageListItem"
+export * from "./patterns/PackageSearchResult"
+export * from "./patterns/PickupCodeDisplay"
+export * from "./patterns/SmsCreditBalance"
+export * from "./patterns/CustomerLookupResult"
+export * from "./patterns/CustomerSummary"
+export * from "./patterns/PaymentSummary"
+export * from "./patterns/ActivityTimeline"
+export * from "./patterns/SmsStatus"
+export * from "./patterns/OperationalStat"
+export * from "./patterns/QuickAction"
+
+// App Shell
+export * from "./shell"
+
