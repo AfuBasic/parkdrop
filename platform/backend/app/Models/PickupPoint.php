2e4eb055314f0c74c4f395bd6bf17ac79ada0044
Afuwape Babatunde <afutunde@gmail.com>
2026-09-19 14:28:11 +0100

    Update PickupPoint.php

--- platform/backend/app/Models/PickupPoint.php
+++ platform/backend/app/Models/PickupPoint.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PickupPoint extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'public_id',
+        'name',
+        'park_name',
+        'status',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
