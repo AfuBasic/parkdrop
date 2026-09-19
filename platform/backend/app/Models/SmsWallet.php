152e46ae396c59fd8909bb4067408bc8b7969312
Afuwape Babatunde <afutunde@gmail.com>
2026-09-19 14:28:11 +0100

    Update SmsWallet.php

--- platform/backend/app/Models/SmsWallet.php
+++ platform/backend/app/Models/SmsWallet.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class SmsWallet extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'balance',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(SmsCreditTransaction::class);
+    }
 }
