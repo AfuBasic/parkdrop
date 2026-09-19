59559afae12b357077cf6bd3377d8d1089c10651
Afuwape Babatunde <afutunde@gmail.com>
2026-09-19 14:28:11 +0100

    Update SmsCreditTransaction.php

--- platform/backend/app/Models/SmsCreditTransaction.php
+++ platform/backend/app/Models/SmsCreditTransaction.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SmsCreditTransaction extends Model
 {
-    //
+    protected $fillable = [
+        'sms_wallet_id',
+        'amount',
+        'type',
+        'reference_type',
+        'reference_id',
+    ];
+
+    public function wallet()
+    {
+        return $this->belongsTo(SmsWallet::class, 'sms_wallet_id');
+    }
 }
