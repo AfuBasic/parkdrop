d37d0e31266e05fb7f0ddc162355541cfc131d2b
Afuwape Babatunde <afutunde@gmail.com>
2026-09-19 14:28:12 +0100

    Update 2026_09_19_002128_create_sms_credit_transactions_table.php

--- platform/backend/database/migrations/2026_09_19_002128_create_sms_credit_transactions_table.php
+++ platform/backend/database/migrations/2026_09_19_002128_create_sms_credit_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('sms_credit_transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('sms_wallet_id')->constrained()->cascadeOnDelete();
+            $table->integer('amount');
+            $table->enum('type', ['credit', 'debit']);
+            $table->string('reference_type'); // e.g., 'WELCOME_CREDIT', 'PACKAGE_SMS'
+            $table->string('reference_id')->nullable();
             $table->timestamps();
         });
     }
