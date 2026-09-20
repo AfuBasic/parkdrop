e1a626d7a6b6fb1f9fe6c345a0eb07332b5b2ff8
Afuwape Babatunde <afutunde@gmail.com>
2026-09-20 22:45:29 +0100

    test(frontend): configure unit test runner with fake-indexeddb and jest-dom

---
+++ platform/frontend/src/test/setup.ts
@@ -0,0 +1,10 @@
+import 'fake-indexeddb/auto';
+import { expect, afterEach } from 'vitest';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { cleanup } from '@testing-library/react';
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
