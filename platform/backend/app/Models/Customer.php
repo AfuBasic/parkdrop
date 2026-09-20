b67bc5cb9678f67bd321fc79eea5f390a6df2c86
Afuwape Babatunde <afutunde@gmail.com>
2026-09-20 15:32:37 +0100

    Update Customer model

--- platform/backend/app/Models/Customer.php
+++ platform/backend/app/Models/Customer.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use \Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+    protected $fillable = [
+        'id',
+        'business_id',
+        'name',
+        'phone_display',
+        'phone_normalized',
+        'version',
+    ];
+
+    protected $casts = [
+        'version' => 'integer',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
