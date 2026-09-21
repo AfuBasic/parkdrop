36a865722fef360f90e2a350f323e9268d811b56
Afuwape Babatunde <afutunde@gmail.com>
2026-09-21 11:36:27 +0100

    refactor(design-system): update PageHeader.tsx

--- platform/frontend/src/design-system/shell/PageHeader.tsx
+++ platform/frontend/src/design-system/shell/PageHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { ChevronLeft } from "lucide-react"
-import { Button } from "../components/Button"
+import { Button } from "@/design-system/components/Button"
 import { cn } from "@/lib/utils"
 
 interface PageHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
