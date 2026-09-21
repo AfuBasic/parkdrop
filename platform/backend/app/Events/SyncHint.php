540fa9717a6d578d3501576eaa9a7428c9f083a7
Afuwape Babatunde <afutunde@gmail.com>
2026-09-21 11:36:26 +0100

    refactor(backend): update SyncHint.php

--- platform/backend/app/Events/SyncHint.php
+++ platform/backend/app/Events/SyncHint.php
@@ -2,7 +2,6 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
@@ -24,7 +23,7 @@ class SyncHint implements ShouldBroadcast
             new PrivateChannel('business.'.$this->businessId),
         ];
     }
-    
+
     public function broadcastAs(): string
     {
         return 'sync.hint';
