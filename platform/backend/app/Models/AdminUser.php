ef16e3b8108fd35af12efde208a09dc540ab569a
Afuwape Babatunde <afutunde@gmail.com>
2026-09-25 16:08:19 +0100

    Add HasFactory trait to AdminUser model

--- platform/backend/app/Models/AdminUser.php
+++ platform/backend/app/Models/AdminUser.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class AdminUser extends Authenticatable
 {
+    use HasFactory;
     protected $fillable = ['email', 'email_verified_at', 'is_active', 'last_login_at', 'last_login_ip'];
 }
